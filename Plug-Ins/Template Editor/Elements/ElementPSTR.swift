cd3585ea705a1ab1e7e45b1014f91d3bcc8120e0
Andrew <andrew@digerati.co.nz>
2020-12-03 20:28:09 +1300

    Fix regression with PSTR height

--- Plug-Ins/Template Editor/Elements/ElementPSTR.swift
+++ Plug-Ins/Template Editor/Elements/ElementPSTR.swift
@@ -69,12 +69,14 @@ class ElementPSTR<T: FixedWidthInteger & UnsignedInteger>: CaseableElement {
     }
     
     private func autoRowHeight(_ field: NSTextField) {
+        let outline = self.parentList.controller.dataList!
+        let index = outline.row(for: field)
+        let element = outline.item(atRow: index) as! Element
         let bounds = NSMakeRect(0, 0, field.bounds.size.width-4, CGFloat.greatestFiniteMagnitude)
         let height = Double(field.cell!.cellSize(forBounds: bounds).height) + 1
-        if height != self.rowHeight {
-            self.rowHeight = height
+        if height != element.rowHeight {
+            element.rowHeight = height
             // Notify the outline view
-            let outline = self.parentList.controller.dataList!
             outline.noteHeightOfRows(withIndexesChanged: [outline.row(for: field)])
         }
     }
