80052c8ecbd14be2a076248fabfe80b60c48900b
Andrew <andrew@digerati.co.nz>
2021-01-18 19:39:47 +1300

    Improve KRID performance

--- Plug-Ins/Template Editor/Elements/ElementKRID.swift
+++ Plug-Ins/Template Editor/Elements/ElementKRID.swift
@@ -5,13 +5,30 @@ class ElementKRID: KeyElement, GroupElement {
     
     override func configure() throws {
         self.rowHeight = 18
-        try self.readCases()
         // Get the current resource id
-        caseEl = self.caseMap[String(self.parentList.controller.resource.id)]
+        let rID = String(self.parentList.controller.resource.id)
+        // Read CASEs
+        while let el = self.parentList.pop("CASE") as? ElementCASE {
+            try el.configure(for: self)
+            if el.displayValue == rID {
+                caseEl = el
+            }
+        }
         guard caseEl != nil else {
+            throw TemplateError.invalidStructure(self, NSLocalizedString("No ‘CASE’ for this resource id.", comment: ""))
+        }
+        // Read KEYBs
+        while let keyB = self.parentList.pop("KEYB") as? ElementKEYB {
+            keyB.subElements = try parentList.subList(for: keyB)
+            let vals = keyB.label.components(separatedBy: ",")
+            if vals.contains(rID) {
+                try keyB.subElements.configure()
+                self.currentSection = keyB
+            }
+        }
+        guard self.currentSection != nil else {
             throw TemplateError.invalidStructure(self, NSLocalizedString("No ‘KEYB’ for this resource id.", comment: ""))
         }
-        self.currentSection = self.keyedSections[caseEl]
         self.parentList.insert(self.currentSection)
     }
     
