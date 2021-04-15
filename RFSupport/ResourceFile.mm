a21be6bde3364e1d6b88f0a143b476dd1a1dec46
Andrew <andrew@digerati.co.nz>
2021-04-15 14:46:30 +1200

    Add attributes to type code

--- RFSupport/ResourceFile.mm
+++ RFSupport/ResourceFile.mm
@@ -17,10 +17,15 @@
     if (format) *format = (ResourceFileFormat)gFile.current_format();
     NSMutableArray *resources = [NSMutableArray new];
     for (auto typeList : gFile.types()) {
+        auto typeString = typeList->code();
+        // Type attributes are a feature of the extended format. For preliminary support, just combine these into the type code.
+        for (auto attribute : typeList->attributes()) {
+            typeString += ":" + attribute.first + "=" + attribute.second;
+        }
+        NSString *type = [NSString stringWithUTF8String:typeString.c_str()];
         for (auto resource : typeList->resources()) {
             // create the resource & add it to the array
             NSString    *name = [NSString stringWithUTF8String:resource->name().c_str()];
-            NSString    *type = [NSString stringWithUTF8String:typeList->code().c_str()];
             NSData      *data = [NSData dataWithBytes:resource->data()->get()->data()+resource->data()->start() length:resource->data()->size()];
             Resource *r = [[Resource alloc] initWithType:type id:resource->id() name:name attributes:0 data:data];
             [resources addObject:r];
@@ -33,12 +38,18 @@
 {
     graphite::rsrc::file gFile = graphite::rsrc::file();
     for (Resource *resource in resources) {
-        std::string name([resource.name UTF8String]);
-        std::string type([resource.type UTF8String]);
+        std::string name(resource.name.UTF8String);
+        std::string type([resource.type substringToIndex:4].UTF8String);
         char *first = (char *)resource.data.bytes; // Bytes pointer should only be accessed once
         std::vector<char> buffer(first, first+resource.data.length);
         graphite::data::data data(std::make_shared<std::vector<char>>(buffer), resource.data.length);
-        gFile.add_resource(type, resource.id, name, std::make_shared<graphite::data::data>(data));
+        std::map<std::string, std::string> attributes;
+        NSString *attString = [resource.type substringFromIndex:5];
+        for (NSString *attribute in [attString componentsSeparatedByString:@":"]) {
+            NSArray<NSString *> *parts = [attribute componentsSeparatedByString:@"="];
+            attributes.insert(std::make_pair(std::string(parts[0].UTF8String), std::string(parts[1].UTF8String)));
+        }
+        gFile.add_resource(type, resource.id, name, std::make_shared<graphite::data::data>(data), attributes);
     }
     try {
         gFile.write(url.fileSystemRepresentation, (graphite::rsrc::file::format)format);
