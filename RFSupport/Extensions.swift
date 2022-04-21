f3965a5920539d3fa870547494a0fa4fd67be556
Andrew <andrew@digerati.co.nz>
2022-04-21 14:46:42 +1200

    Replace deprecated carbon functions

--- RFSupport/Extensions.swift
+++ RFSupport/Extensions.swift
@@ -1,12 +1,32 @@
 import Foundation
 
-// Allow easy translation between String and FourCharCode
+// Allow easy translation between String and FourCharCode, in the same manner as classic OSTypes
 public extension FourCharCode {
     var stringValue: String {
-        return UTCreateStringForOSType(self).takeRetainedValue() as String
+        guard self != 0 else {
+            return ""
+        }
+        let bytes = [
+            UInt8(self >> 24),
+            UInt8(self >> 16 & 0xFF),
+            UInt8(self >> 8 & 0xFF),
+            UInt8(self & 0xFF)
+        ]
+        return String(bytes: bytes, encoding: .macOSRoman) ?? ""
     }
     init(_ string: String) {
-        self = UTGetOSTypeFromString(string as CFString)
+        self = 0
+        guard string != "" else {
+            return
+        }
+        var bytes: [UInt8] = [0,0,0,0]
+        let max = Swift.min(string.count, 4)
+        var used = 0
+        var range = string.startIndex..<string.endIndex
+        _ = string.getBytes(&bytes, maxLength: max, usedLength: &used, encoding: .macOSRoman, range: range, remaining: &range)
+        if used == max {
+            self = bytes.reduce(0) { $0 << 8 | Self($1) }
+        }
     }
 }
 
