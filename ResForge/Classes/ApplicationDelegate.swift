14f7c0dbc12f27a3483313e44ade0bc88ec2c443
Andrew <andrew@digerati.co.nz>
2022-07-26 17:09:03 +1200

    Draft favourite resource types

--- ResForge/Classes/ApplicationDelegate.swift
+++ ResForge/Classes/ApplicationDelegate.swift
@@ -5,22 +5,11 @@ import RFSupport
 class ApplicationDelegate: NSObject, NSApplicationDelegate {
     static let githubURL = "https://github.com/andrews05/ResForge"
     
-    // Don't configure prefs controller until needed
-    private lazy var prefsController: NSWindowController = {
-        ValueTransformer.setValueTransformer(LaunchActionTransformer(), forName: .launchActionTransformerName)
-        let prefs = NSWindowController(windowNibName: "PrefsWindow")
-        prefs.window?.center()
-        return prefs
-    }()
+    private lazy var prefsController = PrefsController()
     
     override init() {
         NSApp.registerServicesMenuSendTypes([.string], returnTypes: [.string])
-        UserDefaults.standard.register(defaults: [
-            RFDefaults.confirmChanges: false,
-            RFDefaults.deleteResourceWarning: true,
-            RFDefaults.launchAction: RFDefaults.LaunchActions.displayOpenPanel,
-            RFDefaults.showSidebar: true
-        ])
+        RFDefaults.register()
     }
     
     func applicationDidFinishLaunching(_ notification: Notification) {
@@ -40,9 +29,9 @@ class ApplicationDelegate: NSObject, NSApplicationDelegate {
     func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
         let launchAction = UserDefaults.standard.string(forKey: RFDefaults.launchAction)
         switch launchAction {
-        case RFDefaults.LaunchActions.openUntitledFile:
+        case RFDefaults.LaunchAction.OpenUntitledFile.rawValue:
             return true
-        case RFDefaults.LaunchActions.displayOpenPanel:
+        case RFDefaults.LaunchAction.DisplayOpenPanel.rawValue:
             NSDocumentController.shared.openDocument(sender)
             return false
         default:
--- ResForge/Classes/PrefsController.swift
+++ ResForge/Classes/PrefsController.swift
@@ -1,25 +1,70 @@
-import Foundation
+import Cocoa
+
+class PrefsController: NSWindowController {
+    @IBOutlet var favoriteTypes: NSArrayController!
+
+    override var windowNibName: NSNib.Name? {
+        "PrefsWindow"
+    }
+
+    override func windowWillLoad() {
+        ValueTransformer.setValueTransformer(LaunchActionTransformer(), forName: .launchActionTransformerName)
+    }
+
+    override func windowDidLoad() {
+        window?.center()
+    }
+
+    @IBAction func add(_ sender: Any) {
+        favoriteTypes.addObject("xxxx")
+    }
+
+    @IBAction func remove(_ sender: Any) {
+        favoriteTypes.remove(contentsOf: favoriteTypes.selectedObjects)
+    }
+}
+
+// MARK: -
 
 struct RFDefaults {
     static let confirmChanges = "ConfirmChanges"
-    static let deleteResourceWarning =  "DeleteResourceWarning"
+    static let deleteResourceWarning = "DeleteResourceWarning"
     static let launchAction = "LaunchAction"
     static let showSidebar = "ShowSidebar"
-    struct LaunchActions {
-        static let openUntitledFile = "OpenUntitledFile"
-        static let displayOpenPanel = "DisplayOpenPanel"
-        static let none = "None"
+    static let favoriteTypes = "FavoriteTypes"
+
+    enum LaunchAction: String, CaseIterable {
+        case None
+        case OpenUntitledFile
+        case DisplayOpenPanel
+    }
+
+    static func register() {
+        UserDefaults.standard.register(defaults: [
+            confirmChanges: false,
+            deleteResourceWarning: true,
+            launchAction: LaunchAction.DisplayOpenPanel.rawValue,
+            showSidebar: true,
+            favoriteTypes: [
+                "PICT",
+                "snd ",
+                "STR ",
+                "STR#",
+                "TMPL",
+            ]
+        ])
+    }
+}
+
+extension RawRepresentable where Self: CaseIterable, RawValue: Equatable {
+    /// Returns the index of a raw value in the enum.
+    static func index(of rawValue: RawValue) -> AllCases.Index? {
+        return allCases.firstIndex { $0.rawValue == rawValue }
     }
 }
 
 // Transform launch action matrix index to string constants
 class LaunchActionTransformer: ValueTransformer {
-    private static let launchActions: [String] = [
-        RFDefaults.LaunchActions.none,
-        RFDefaults.LaunchActions.openUntitledFile,
-        RFDefaults.LaunchActions.displayOpenPanel
-    ]
-        
     static override func transformedValueClass() -> AnyClass {
         return NSNumber.self
     }
@@ -29,11 +74,11 @@ class LaunchActionTransformer: ValueTransformer {
     }
     
     override func transformedValue(_ value: Any?) -> Any? {
-        return Self.launchActions.firstIndex(of: value as! String)
+        return RFDefaults.LaunchAction.index(of: value as! String)
     }
     
     override func reverseTransformedValue(_ value: Any?) -> Any? {
-        return Self.launchActions[value as! Int]
+        return RFDefaults.LaunchAction.allCases[value as! Int].rawValue
     }
 }
 
