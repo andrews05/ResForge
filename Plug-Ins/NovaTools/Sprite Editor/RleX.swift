deea9b18f3dcbed83436ff0a4ec3896d2b342382
Andrew <andrew@digerati.co.nz>
2022-09-18 16:25:47 +1200

    RleX writer

--- Plug-Ins/NovaTools/Sprite Editor/RleX.swift
+++ Plug-Ins/NovaTools/Sprite Editor/RleX.swift
@@ -38,7 +38,7 @@ class RleX: SpriteWorld {
                 if pos + advance > frameWidth * frameHeight {
                     throw SpriteError.invalid
                 }
-                let rgb = yuv2rgb(luma: luma, blueDiff: blueDiff, redDiff: redDiff)
+                let rgb = yuv2rgb(y: luma, cb: blueDiff, cr: redDiff)
                 let val = (UInt32(rgb.r) << 24 | UInt32(rgb.g) << 16 | UInt32(rgb.b) << 8 | UInt32(alpha)).bigEndian
                 // When reading as a sheet we need to adhere to the lineAdvance when completing each line,
                 // rather than just assigning the full count in one go
@@ -56,18 +56,79 @@ class RleX: SpriteWorld {
         }
     }
     
-    private func yuv2rgb(luma: UInt8, blueDiff: UInt8, redDiff: UInt8) -> (r: UInt8, g: UInt8, b: UInt8) {
-        let Y = Double(luma)
-        let Cb = Double(blueDiff) - 128
-        let Cr = Double(redDiff) - 128
-        let r = UInt8(clamping: Int(Y + 1.402000 * Cr))
-        let g = UInt8(clamping: Int(Y - 0.344136 * Cb - 0.714136 * Cr))
-        let b = UInt8(clamping: Int(Y + 1.772000 * Cb))
+    override func writeFrame(_ frame: NSBitmapImageRep, dither: Bool = false) {
+        var framePointer = frame.bitmapData!
+        var yuv: (y: UInt8, cb: UInt8, cr: UInt8) = (0, 128, 128)
+        var alpha: UInt8 = 255
+        var count = 0
+        for _ in 0..<(frameWidth * frameHeight) {
+            let nextAlpha = framePointer[3]
+            
+            // Don't bother reading colour value when alpha is zero
+            if nextAlpha > 0 {
+                let nextYuv = rgb2yuv(r: framePointer[0], g: framePointer[1], b: framePointer[2])
+                if nextYuv != yuv {
+                    self.writeAdvance(count)
+                    count = 0
+                    if nextYuv.y != yuv.y {
+                        writer.write(RleXOp.luma.rawValue)
+                        writer.write(nextYuv.y)
+                    }
+                    if nextYuv.cb != yuv.cb {
+                        writer.write(RleXOp.blueDiff.rawValue)
+                        writer.write(nextYuv.cb)
+                    }
+                    if nextYuv.cr != yuv.cr {
+                        writer.write(RleXOp.redDiff.rawValue)
+                        writer.write(nextYuv.cr)
+                    }
+                    yuv = nextYuv
+                }
+            }
+            
+            if nextAlpha != alpha {
+                self.writeAdvance(count)
+                count = 0
+                writer.write(RleXOp.alpha.rawValue)
+                writer.write(nextAlpha)
+                alpha = nextAlpha
+            }
+            
+            framePointer = framePointer.advanced(by: 4)
+            count += 1
+        }
+        
+        self.writeAdvance(count)
+        writer.write(RleXOp.frameEnd.rawValue)
+    }
+    
+    private func writeAdvance(_ count: Int) {
+        if count > 127 {
+            writer.write(RleXOp.advance.rawValue)
+            writer.write(UInt32(count))
+        } else if count > 0 {
+            writer.write(RleXOp.shortAdvance(count).rawValue)
+        }
+    }
+    
+    private func yuv2rgb(y: UInt8, cb: UInt8, cr: UInt8) -> (r: UInt8, g: UInt8, b: UInt8) {
+        let y = Double(y)
+        let cb = Double(cb) - 128
+        let cr = Double(cr) - 128
+        let r = UInt8(clamping: Int(y + 1.402000 * cr))
+        let g = UInt8(clamping: Int(y - 0.344136 * cb - 0.714136 * cr))
+        let b = UInt8(clamping: Int(y + 1.772000 * cb))
         return (r, g, b)
     }
     
-    override func writeFrame(_ frame: NSBitmapImageRep, dither: Bool = false) {
-        // TODO
+    private func rgb2yuv(r: UInt8, g: UInt8, b: UInt8) -> (y: UInt8, cb: UInt8, cr: UInt8) {
+        let r = Double(r)
+        let g = Double(g)
+        let b = Double(b)
+        let y = UInt8(clamping: Int(0.299000 * r + 0.587000 * g + 0.114000 * b))
+        let cb = UInt8(clamping: Int(128 - 0.168736 * r - 0.331264 * g + 0.500000 * b))
+        let cr = UInt8(clamping: Int(128 + 0.500000 * r - 0.418688 * g - 0.081312 * b))
+        return (y, cb, cr)
     }
 }
 
