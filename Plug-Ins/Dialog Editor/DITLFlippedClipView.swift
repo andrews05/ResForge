28750e58233ca9580a531e13b2ab6335114aeed2
Uli Kusterer <ulivc@zathras.de>
2024-02-14 18:24:54 +0100

    Use spaces for indentation.

--- Plug-Ins/Dialog Editor/DITLFlippedClipView.swift
+++ Plug-Ins/Dialog Editor/DITLFlippedClipView.swift
@@ -3,12 +3,12 @@ import Cocoa
 
 /// View that makes our document view top-left aligned inside the scroll view:
 class DITLFlippedClipView : NSClipView {
-	public override var isFlipped: Bool {
-		get {
-			return true
-		}
-		set(newValue) {
-			
-		}
-	}
+    public override var isFlipped: Bool {
+        get {
+            return true
+        }
+        set(newValue) {
+            
+        }
+    }
 }
